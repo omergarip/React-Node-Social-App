48ef3a486e79878f2079696be2664fd785cff1cb
Omer Garip <omergarip95@gmail.com>
2020-02-26 15:05:42 -0500

    Installing express to create a server

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('hello world from node js');
+});
+
+const port = 8080;
+
+app.listen(port, () => {
+    console.log(`A Node Js API is listening on port ${port}`);
+});
