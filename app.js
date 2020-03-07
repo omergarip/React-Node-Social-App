bd2661701a767f0e018e50ab7be6d0437b444e7e
Omer <omergarip95@gmail.com>
2020-03-06 22:45:59 -0500

    Creating post model

--- app.js
+++ app.js
@@ -1,33 +1,29 @@
-const dotenv   = require('dotenv');
-const express  = require('express');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+const express = require('express');
 const mongoose = require('mongoose');
-const morgan   = require('morgan');
+const morgan = require('morgan');
 
 const app = express();
 dotenv.config();
 
 // Connecting to DB
-mongoose
-    .connect(
-        process.env.MONGO_URI, 
-        {useNewUrlParser: true}
-    )
-    .then(() => console.log('DB Connected'));
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true }).then(() => console.log('DB Connected'));
 
-mongoose.connection.on('error', err => {
-    console.log(`DB connection error: ${ err.message }`)
+mongoose.connection.on('error', (err) => {
+	console.log(`DB connection error: ${err.message}`);
 });
-
 
 // Importing routes
 const postRoutes = require('./routes/post');
 
 // Middleware
+app.use(bodyParser.json());
 app.use(morgan('dev'));
 app.use('/', postRoutes);
 
 const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
-    console.log(`A Node Js API is listening on port ${port}`);
+	console.log(`A Node Js API is listening on port ${port}`);
 });
