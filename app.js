d2fd4857e998e38751d850f6d2492c5433835130
Omer <omergarip95@gmail.com>
2020-03-17 01:24:43 -0400

    User signup functionality is completed

--- app.js
+++ app.js
@@ -9,7 +9,9 @@ const app = express();
 dotenv.config();
 
 // Connecting to DB
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true }).then(() => console.log('DB Connected'));
+mongoose
+	.connect(process.env.MONGO_URI, { useUnifiedTopology: true, useNewUrlParser: true })
+	.then(() => console.log('DB Connected'));
 
 mongoose.connection.on('error', (err) => {
 	console.log(`DB connection error: ${err.message}`);
@@ -17,12 +19,14 @@ mongoose.connection.on('error', (err) => {
 
 // Importing routes
 const postRoutes = require('./routes/post');
+const authRoutes = require('./routes/auth');
 
 // Middleware
 app.use(bodyParser.json());
 app.use(expressValidator());
 app.use(morgan('dev'));
 app.use('/', postRoutes);
+app.use('/', authRoutes);
 
 const port = process.env.PORT || 8080;
 
